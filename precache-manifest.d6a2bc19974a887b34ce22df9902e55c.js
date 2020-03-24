4588c75f453f08f5368798fa1287e617e61a462c
Tinh Phan <minhtinh885@gmail.com>
2020-03-24 20:00:54 +0700

    Updates

---
+++ precache-manifest.d6a2bc19974a887b34ce22df9902e55c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85668a85833c6393e67e30d0df5f58a1",
+    "url": "/tinh-phan/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b84508e6d1ff0ad43ddb",
+    "url": "/tinh-phan/monsters-rolodex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "78dd2bc8a741db6828c1",
+    "url": "/tinh-phan/monsters-rolodex/static/js/2.dc0b79fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tinh-phan/monsters-rolodex/static/js/2.dc0b79fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84508e6d1ff0ad43ddb",
+    "url": "/tinh-phan/monsters-rolodex/static/js/main.afca55da.chunk.js"
+  },
+  {
+    "revision": "6b6af7947e37a0371188",
+    "url": "/tinh-phan/monsters-rolodex/static/js/runtime-main.c5615edb.js"
+  }
+]);
